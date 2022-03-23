55ec2b191566847a1e74ddafc4c4d0b828ad5cfd
amir yehuda <amir@moveohls.co.il>
2022-03-23 10:33:27 +0200

    wating for icins

--- src/components/Header/Notifications.tsx
+++ src/components/Header/Notifications.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
+// import { ReactComponent as Notifications } from "../../assets/Icons/******* */.svg";
 
 export default function AccessibilityTooltips() {
   return (
