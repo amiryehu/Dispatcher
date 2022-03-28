0fc1aabf98aeed126aacdf0afe087be1cbbf94ba
amir yehuda <amir@moveohls.co.il>
2022-03-28 11:11:16 +0300

    create data cards

---
+++ src/components/DataCard/DataCard-style.ts
@@ -0,0 +1,3 @@
+import styled from "styled-components";
+import { colors, cardrRadius } from "../../Utils/Constances";
+
---
+++ src/components/DataCard/DataCard.tsx
@@ -0,0 +1,16 @@
+import { type } from "os";
+import React from "react";
+
+type IDataCard = {
+    title: string
+}
+
+const DataCard = (props: IDataCard) => {
+    const {title} = props;
+    
+    return (
+        <div>amir</div>
+    )
+}
+
+export default DataCard;
