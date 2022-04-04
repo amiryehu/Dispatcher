ac6789b1943faae149ed68accb948b44017e06f0
amir yehuda <amir@moveohls.co.il>
2022-04-04 10:57:19 +0300

    no message

--- src/components/MainContent/MainContent-style.ts
+++ src/components/MainContent/MainContent-style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const MainContentContainer = styled.div`
     width: 90%;
-    height: 1200px;
+    height: 100vh;
     margin-left: 5%;
     margin-right: 5%;
     display: flex;
