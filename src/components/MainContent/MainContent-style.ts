3b28b29fc2355fd5021f4eb4dd83a8104c697bc1
amir yehuda <amir@moveohls.co.il>
2022-04-11 19:54:22 +0300

    fix height main content

--- src/components/MainContent/MainContent-style.ts
+++ src/components/MainContent/MainContent-style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const AllBody = styled.div`
     width: 100%;
-    height: 1050px;
+    height: 110vh;
     display: flex;
     flex-direction: column;
     justify-content: center;
